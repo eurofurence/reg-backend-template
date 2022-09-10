c7fbabbf004457f8f6a07f53311af9386701afdf
Noroth <ludwig.bedacht@web.de>
2022-09-10 20:21:04 +0200

    fixes and improments to main file and server handling

--- render.sh
+++ render.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd ../reg-backend-template-test
-generator-cli --create --generator=../reg-backend-template --target=.
-generator-cli --render --generator=../reg-backend-template --target=.
+go-generator-cli --create --generator=../reg-backend-template --target=.
+go-generator-cli --render --generator=../reg-backend-template --target=.
